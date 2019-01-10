26e146918e3e2d59fb20c7b1fded30076d3e0894
Владимир <wolfmariev@gmail.com>
2019-01-10 11:48:40 +0500

    Старт2

---
+++ Assets/Models/HealthPotionSmall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Models
+{
+    public class HealthPotionSmall:MonoBehaviour
+    {
+        [SerializeField]
+        public float Points;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "Player")
+            {
+                //Анимация подбора
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
---
+++ Assets/Models/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Models;
+
+public class Player : MonoBehaviour
+{
+    //public float InputX;
+    //public float InputY;
+    //public bool IsWayDiagon;
+
+    public float Health;
+    private Rigidbody2D rigidbody;
+
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = 10;
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Horizontal")|| Input.GetButton("Vertical"))
+        {
+            float x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * Speed;
+            float y = Input.GetAxisRaw("Vertical") * Time.deltaTime * Speed;
+
+            if (Input.GetButton("Horizontal") && Input.GetButton("Vertical"))
+            {
+                x = x * 0.7f;
+                y = y * 0.7f;
+            }
+
+            Vector3 direction = new Vector3(x, y);
+            Vector3 newPosition = direction + transform.position;
+            Debug.Log(Vector3.Distance(newPosition, transform.position) / Time.deltaTime);
+            rigidbody.MovePosition(newPosition);
+
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="HealthPotion")
+        {
+            //Анимация лечения
+            Health += collision.GetComponent<HealthPotionSmall>().Points;
+            Debug.Log("HP +" + collision.GetComponent<HealthPotionSmall>().Points);
+            Debug.Log("HP ="+ Health);
+        }
+    }
+}
